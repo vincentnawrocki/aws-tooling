bc66d504cedb7a3d5a5fcf3b117099e6cf8d8e54
vincent <nawrocki.vincent@gmail.com>
2019-08-24 13:03:43 +0200

    init commit for script

---
+++ enable_ebs_default_encrypt/enable_ebs_default_encrypt.py
@@ -0,0 +1,93 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+import argparse
+import logging
+import tqdm
+import time
+
+class TqdmLoggingHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.tqdm.write(msg)
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+def enable_ebs_default_encryption(role: str, account_file: str):
+
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
+    log.addHandler(TqdmLoggingHandler())
+
+    sts_client = boto3.client('sts')
+    failure_list = []
+
+    with open(account_file) as file:
+        account_list = json.load(file)
+
+    log.info(f"Default ebs encryption will be activated on all regions for the list of account(s): {account_list['accounts']}")
+
+    for account in tqdm.tqdm(account_list['accounts'], desc="Accounts"):
+        role_arn = f"arn:aws:iam::{account}:role/{role}"
+
+        try:
+            assume_role = sts_client.assume_role(RoleArn=role_arn, RoleSessionName="get_all_regions", DurationSeconds=3600)
+            session = boto3.Session(
+                aws_access_key_id=assume_role['Credentials']['AccessKeyId'],
+                aws_secret_access_key=assume_role['Credentials']['SecretAccessKey'],
+                aws_session_token=assume_role['Credentials']['SessionToken'],
+                region_name="us-east-1"
+            )
+        except ClientError as error:
+            log.error(f"Failed to assume role during regions retrieval {role_arn} : {error}")
+
+        try:
+            ec2_client = session.client('ec2', region_name='us-east-1')
+            aws_regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
+            log.info(f"Regions retrived using role {role_arn} : {aws_regions}")
+        except ClientError as error:
+            log.error(f"Failed to get regions : {error}")
+
+        for region in tqdm.tqdm(aws_regions, desc="Regions"):
+            try:
+                assume_role = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=f"enable_ebs_encryption_{region}")
+                session = boto3.Session(
+                    aws_access_key_id=assume_role['Credentials']['AccessKeyId'],
+                    aws_secret_access_key=assume_role['Credentials']['SecretAccessKey'],
+                    aws_session_token=assume_role['Credentials']['SessionToken'],
+                    region_name='us-east-1'
+                )
+            except ClientError as error:
+                log.error(f"Failed to assume role {role_arn} : {error}")
+
+            try:
+                ec2_client = session.client('ec2')
+                encryption_result = ec2_client.enable_ebs_encryption_by_default()
+                if encryption_result is False:
+                    failure_list.append(f"{account}/{region}")
+                else:
+                    log.info(f"EBS default encryption enabled on {account}/{region}")
+            except ClientError as error:
+                log.error(f"Error during EBS default encryption setting activation: {error}")
+
+    # Print error list
+    if failure_list:
+        log.error(f"Activation of EBS default encryption failed on account/region: {failure_list}")
+    else:
+        log.info("No error during the process")
+
+enable_ebs_default_encryption(role="ebs_default_encryptioner", account_file="accounts.json")
+
+# def parser():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(name="accounts_file", help="", required=True)
+
+# if __name__ == "__main__":
+#     parser()
