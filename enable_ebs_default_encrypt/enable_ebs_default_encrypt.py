df23d3c8247def23b767b265cf1ea0e8f637d10f
vincent <nawrocki.vincent@gmail.com>
2019-08-24 18:32:40 +0200

    made action function independant from accounts/regions crawling

--- enable_ebs_default_encrypt/enable_ebs_default_encrypt.py
+++ enable_ebs_default_encrypt/enable_ebs_default_encrypt.py
@@ -30,18 +30,22 @@ class TqdmLoggingHandler(logging.Handler):
         except:
             self.handleError(record)
 
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+LOG.addHandler(TqdmLoggingHandler())
 
-def enable_ebs_default_encryption(role: str, account_file: str):
-    """Enable default EBS encryption on all regions for all accounts found in input file.
+def all_region_modifier(role: str, account_file: str, action):
+    """all_region_modifier [summary]
 
     Arguments:
         role {str} -- [description]
         account_file {str} -- [description]
+        action {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
 
     """
-    log = logging.getLogger(__name__)
-    log.setLevel(logging.INFO)
-    log.addHandler(TqdmLoggingHandler())
 
     sts_client = boto3.client('sts')
     failure_list = []
@@ -49,7 +53,7 @@ def enable_ebs_default_encryption(role: str, account_file: str):
     with open(account_file) as file:
         account_list = json.load(file)
 
-    log.info(
+    LOG.info(
         f"Default ebs encryption will be activated on all regions for the list of account(s): {account_list['accounts']}")
 
     for account in tqdm.tqdm(account_list['accounts'], desc="Accounts"):
@@ -65,16 +69,16 @@ def enable_ebs_default_encryption(role: str, account_file: str):
                 region_name="us-east-1"
             )
         except ClientError as error:
-            log.error(
+            LOG.error(
                 f"Failed to assume role during regions retrieval {role_arn} : {error}")
 
         try:
             ec2_client = session.client('ec2', region_name='us-east-1')
             aws_regions = [region['RegionName']
                            for region in ec2_client.describe_regions()['Regions']]
-            log.info(f"Regions retrived using role {role_arn} : {aws_regions}")
+            LOG.info(f"Regions retrived using role {role_arn} : {aws_regions}")
         except ClientError as error:
-            log.error(f"Failed to get regions : {error}")
+            LOG.error(f"Failed to get regions : {error}")
 
         for region in tqdm.tqdm(aws_regions, desc="Regions"):
             try:
@@ -87,27 +91,40 @@ def enable_ebs_default_encryption(role: str, account_file: str):
                     region_name='us-east-1'
                 )
             except ClientError as error:
-                log.error(f"Failed to assume role {role_arn} : {error}")
+                LOG.error(f"Failed to assume role {role_arn} : {error}")
 
-            try:
-                ec2_client = session.client('ec2')
-                encryption_result = ec2_client.enable_ebs_encryption_by_default()
-                if encryption_result is False:
-                    failure_list.append(f"{account}/{region}")
-                else:
-                    log.info(
-                        f"EBS default encryption enabled on {account}/{region}")
-            except ClientError as error:
-                log.error(
-                    f"Error during EBS default encryption setting activation: {error}")
+            failure_list += action(role=role, session=session, account=account, region=region)
 
     # Print error list
     if failure_list:
-        log.error(
-            f"Activation of EBS default encryption failed on account/region: {failure_list}")
+        LOG.error(f"Failures encountered applying change on account/region: {failure_list}")
     else:
-        log.info("No error during the process")
+        LOG.info("No error during the process")
+
+def enable_ebs_default_encryption(role: str, session: boto3.Session, account: str, region: str):
+    """Enable default EBS encryption on all regions for all accounts found in input file.
+
+    Arguments:
+        role {str} -- [description]
+        account_file {str} -- [description]
+
+    """
+
+    local_failure_list = []
+
+    try:
+        ec2_client = session.client('ec2')
+        encryption_result = ec2_client.enable_ebs_encryption_by_default()
+        if encryption_result is False:
+            local_failure_list.append(f"{account}/{region}")
+        else:
+            LOG.info(
+                f"EBS default encryption enabled on {account}/{region}")
+    except ClientError as error:
+        LOG.error(
+            f"Error during EBS default encryption setting activation: {error}")
+
+    return local_failure_list
 
 
-enable_ebs_default_encryption(
-    role="ebs_default_encryptioner", account_file="accounts.json")
+all_region_modifier(role="ebs_default_encryptioner", account_file="accounts.json", action=enable_ebs_default_encryption)
